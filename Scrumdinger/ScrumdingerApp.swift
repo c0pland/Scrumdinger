c9a218c3f38b5f99567911b69c8cc93ff8df7eb5
Bogdan Benner <c0pland@icloud.com>
2022-10-23 15:45:55 +0300

    add persistance

--- Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/ScrumdingerApp.swift
@@ -9,14 +9,28 @@ import SwiftUI
 
 @main
 struct ScrumdingerApp: App {
-	@State private var scrums = DailyScrum.sampleData
-	
-    var body: some Scene {
-        WindowGroup {
+	@StateObject private var store = ScrumStore()
+	var body: some Scene {
+		WindowGroup {
 			NavigationView {
-				ScrumsView(scrums: $scrums)
+				ScrumsView(scrums: $store.scrums) {
+					ScrumStore.save(scrums: store.scrums) { result in
+						if case .failure(let failure) = result {
+							fatalError(failure.localizedDescription)
+						}
+					}
+				}
 			}
-        }
-    }
+			.onAppear {
+				ScrumStore.load { result in
+					switch result {
+					case .success(let scrums):
+						store.scrums = scrums
+					case .failure(let error):
+						fatalError(error.localizedDescription)
+					}
+				}
+			}
+		}
+	}
 }
-
